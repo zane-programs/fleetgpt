47c1ed68d96db57a8b667de86d1c527b526ce26b
Zane St. John <zstjohn@stanford.edu>
2023-04-19 10:57:35 -0700

    add vercel & force dark mode

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,7 @@
 import { extendTheme } from "@chakra-ui/theme-utils";
 
 const theme = extendTheme({
+  config: { initialColorMode: "dark", useSystemColorMode: false },
   fonts: {
     heading: `'ChatGPTFont', serif`,
     body: `'ChatGPTFont', serif`,
